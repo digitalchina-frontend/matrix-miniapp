e808ccc7569b025e94359c68de0b24cbf3fc8a69
LAPTOP-SFJA1GSU\hw <285961971@qq.com>
2023-02-03 09:46:09 +0800

    📦 NEW: 添加景区列表页面和景区card组件

--- miniprogram/pages/scenicSpotList/index.js
+++ miniprogram/pages/scenicSpotList/index.js
@@ -5,9 +5,10 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    list:[],
+    done:false
   },
-
+  page:1,
   /**
    * 生命周期函数--监听页面加载
    */
@@ -20,6 +21,7 @@ Page({
    */
   onReady() {
 
+    this.getList()
   },
 
   /**
@@ -62,5 +64,48 @@ Page({
    */
   onShareAppMessage() {
 
+  },
+  getList(){
+    return new Promise((resolve,reject)=>{
+      wx.showLoading({
+        title: '加载中',
+      })
+      setTimeout(() => {
+        const list=this.createFakeData((this.page-1)*20+1)
+        resolve(list)
+        this.setData({
+          list:this.data.list.concat(list)
+        })
+        this.page++
+        if(this.page===4){
+          this.setData({done:true})
+        }
+        wx.hideLoading()
+      }, 500);
+    })
+  },
+  createFakeData(startIndex){
+    const fakeData=[]
+    for (let index = 0; index < 20; index++) {
+      fakeData.push({
+        name:'景区'+(startIndex+index),
+        img:'https://picsum.photos/200?'+index,
+        desc:'这是一个景区',
+        tags:['公园','打卡圣地'],
+        price:Math.floor(Math.random()*100),
+        distance:(Math.random()*100).toFixed(2)
+      })
+    }
+    return fakeData
+  },
+  scrolltolower(){
+    if(!this.data.done){
+      console.log(11);
+      this.getList()
+    }
+  },
+  refresh(){
+    this.setData({page:1})
+    this.getList()
   }
 })
