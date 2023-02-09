991519435637db204b9dc44572abb53ca640fd51
zhangctd <zhangctd@digitalchina.com>
2023-02-09 10:42:37 +0800

    📦 NEW: 新增商城首页轮播图
    ✅ Closes: #25

---
+++ miniprogram/pages/banner/components/activeImage/index.js
@@ -0,0 +1,49 @@
+// pages/banner/components/activeImage/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+   
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    imgUrls: [{
+      url: 'https://picsum.photos/200?1',
+      tit: '活动1',
+      price: 43
+    },
+    {
+      url: 'https://picsum.photos/200?2',
+      tit: '活动2',
+      price: 45
+    },
+    {
+      url: 'https://picsum.photos/200?3',
+      tit: '活动3',
+      price: 89
+    },
+    {
+      url: 'https://picsum.photos/200?4',
+      tit: '活动4',
+      price: 56
+    },
+    {
+      url: 'https://picsum.photos/200?5',
+      tit: '活动5',
+      price: 76
+    },
+
+  ],
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
