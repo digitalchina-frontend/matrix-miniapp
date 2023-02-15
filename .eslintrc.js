55a34e42555c6aeb1dfc89ac7e31e1c066f3bcc7
肖子立 <xiaozlb@digitalchina.com>
2023-02-15 16:52:50 +0800

    📦 NEW: 小程序分包架构

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,10 @@
-const reactPaths = ['domains/finance/finance-admin-react/src/**/*.{js,ts,jsx,tsx}'];
-const vuePaths = ['domains/finance/finance-admin-vue3/src/**/*.{js,ts,vue}'];
-const miniAppPaths = ['domains/finance/finance-wechat-app/src/**/*.{js}'];
+const miniAppPaths = ['apps/**/*.{js}'];
 
 module.exports = {
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
   overrides: [
-    {
-      files: reactPaths,
-      ...require('./.eslintrc.react.js'),
-    },
-    {
-      files: vuePaths,
-      ...require('./.eslintrc.vue.js'),
-    },
     {
       files: miniAppPaths,
       ...require('./.eslintrc.miniapp.js'),
